5f064d4ffba29fbf622695c82e92041583a4544d
Aniket Singh <163713137+aniketsingh9310@users.noreply.github.com>
2025-09-22 11:18:10 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
-import { authDataContext } from './authContex'
+import { authDataContext } from './AuthContex.jsx'
 import axios from 'axios'
 import { userDataContext } from './UserContex'
 import { toast } from 'react-hot-toast'
