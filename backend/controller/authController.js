189c1a46da18d1813cddc10356196ff40b7ea5ca
Aniket Singh <163713137+aniketsingh9310@users.noreply.github.com>
2025-09-22 10:45:30 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -23,8 +23,8 @@ export const registration = async (req,res)=>{
         let token = await gentoken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -49,8 +49,8 @@ export const login = async (req,res)=>{
         let token = await gentoken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 1000  
 
         })
@@ -85,8 +85,8 @@ export const googleLogin = async (req,res)=>{
         let token = await gentoken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 69 * 1000
         })
 
@@ -107,8 +107,8 @@ export const adminLogin = async (req,res) =>{
                 let token = await gentoken1(email)
                     res.cookie("token",token,{
                         httpOnly:true,
-                        secure:false,
-                        sameSite:"Strict",
+                        secure:true,
+                        sameSite:"none",
                         maxAge: 1 * 24 * 69 * 1000
                     })
 
