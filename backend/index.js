f0ecd0b686d2e26d267bb7b7e1f81311fc36c514
Aniket Singh <163713137+aniketsingh9310@users.noreply.github.com>
2026-05-01 12:41:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["https://e-commers-1-frontend.onrender.com","https://e-commers-admin.onrender.com"],
+    origin:["https://e-commers-frontend-l8h4.onrender.com","https://e-commers-admin.onrender.com"],
     credentials:true
 }))
 
