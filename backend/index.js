846ab7a293470f6d415e68f0f8665588ac6a3e8c
Aniket Singh <163713137+aniketsingh9310@users.noreply.github.com>
2026-05-06 11:49:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,22 +20,13 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-
-const allowedOrigins = [
-  "https://e-commers-frontend-l8h4.onrender.com", 
-  "https://e-commers-admin-ten.vercel.app"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed"));
-    }
-  },
-  credentials: true
-}));
+    origin:[
+      "https://e-commers-nu-ten.vercel.app",
+      "https://e-commers-admin-ten.vercel.app/"
+    ],
+    credentials:true
+}))
 
 app.get("/health",(req,res)=>{
     return res.json({
